7ee9f666b9305f248ed6b712d9779d60beb86643
Elorucov <elorucov@live.ru>
2023-03-08 21:46:26 +0400

    Prevent service & expired message selection.

--- L2/Controls/ChatViewItem.axaml.cs
+++ L2/Controls/ChatViewItem.axaml.cs
@@ -26,10 +26,16 @@ namespace ELOR.Laney.Controls {
         }
 
         // Необходимо для того, чтобы при ПКМ не пробрасывалось
-        // событие нажатия к ListBox и не выделялось сообщение.
+        // событие нажатия к ListBox и не выделялось сообщение,
+        // а при ЛКМ проверить, можно ли выделить сообщение
+        // (например, оно не сервисное)
         private void ChatViewItem_PointerPressed(object sender, Avalonia.Input.PointerPressedEventArgs e) {
             bool isRight = !e.GetCurrentPoint(this).Properties.IsLeftButtonPressed;
-            if (isRight) e.Handled = true;
+            if (isRight) {
+                e.Handled = true;
+            } else {
+                if (Message.Action != null || Message.IsExpired || Message.TTL > 0) e.Handled = true;
+            }
         }
 
         private void ChatViewItem_Unloaded(object sender, Avalonia.Interactivity.RoutedEventArgs e) {
