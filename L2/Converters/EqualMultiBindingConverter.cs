d2f1919df19348d6a728495bbc0b6919dbc75739
Elorucov <elorucov@live.ru>
2024-10-08 20:24:13 +0400

    Removed debug info.

--- L2/Converters/EqualMultiBindingConverter.cs
+++ L2/Converters/EqualMultiBindingConverter.cs
@@ -9,12 +9,7 @@ namespace ELOR.Laney.Converters {
     // Returns true if all values is same object.
     internal class EqualMultiBindingConverter : IMultiValueConverter {
         public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture) {
-            bool ka = values.All(o => o == values[0]);
-            if (ka) {
-                string str = String.Join(", ", values);
-                Debug.WriteLine($"EqualMultiBindingConverter: {str}");
-            }
-            return ka;
+            return values.All(o => o == values[0]);
         }
     }
 }
