b3403f23305dc7060c8c5ddc5f17ffa789316799
Elorucov <elorucov@live.ru>
2025-02-04 13:07:04 +0400

    Remove old messages from ReceivedMessages list after a certain count.

--- L2/ViewModels/ChatViewModel.cs
+++ L2/ViewModels/ChatViewModel.cs
@@ -654,7 +654,10 @@ namespace ELOR.Laney.ViewModels {
                 }
 
                 bool canAddToDisplayedMessages = DisplayedMessages?.Last?.ConversationMessageId == ReceivedMessages.LastOrDefault()?.ConversationMessageId;
+
+                if (ReceivedMessages.Count >= Constants.MessagesCount) ReceivedMessages.RemoveAt(0);
                 ReceivedMessages.Add(msg);
+
                 if (message.Action != null) ParseActionMessage(message.FromId, message.Action, message.Attachments);
                 // if (!flags.HasFlag(65536)) UpdateSortId(SortId.MajorId, msg.Id);
                 if (msg.SenderId != session.Id) UnreadMessagesCount++;
