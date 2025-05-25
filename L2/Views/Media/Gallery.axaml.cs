fec25004a1e61970b519b3792cc0fe46f1f1319b
Elorucov <elorucov@live.ru>
2025-05-26 01:11:42 +0400

    Updated libraries and fixed photo viewer.

--- L2/Views/Media/Gallery.axaml.cs
+++ L2/Views/Media/Gallery.axaml.cs
@@ -1,10 +1,13 @@
 using Avalonia.Controls;
 using Avalonia.Controls.PanAndZoom;
+using Avalonia.Labs.Controls;
 using Avalonia.Styling;
 using ELOR.Laney.Core;
 using ELOR.Laney.Extensions;
 using ELOR.VKAPILib.Objects;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ELOR.Laney.Views.Media {
@@ -13,8 +16,9 @@ namespace ELOR.Laney.Views.Media {
             new Gallery(items, target).Show();
         }
 
-        List<IPreview> items;
-        IPreview target;
+        List<IPreview> _items;
+        IPreview _target;
+
         public Gallery() {
             InitializeComponent();
             dbg.IsVisible = Settings.ShowDebugInfoInGallery;
@@ -26,27 +30,29 @@ namespace ELOR.Laney.Views.Media {
             dbg.IsVisible = Settings.ShowDebugInfoInGallery;
             RequestedThemeVariant = ThemeVariant.Dark;
 
-            this.items = items;
-            this.target = target;
+            this._items = items;
+            this._target = target;
 
             Activated += Gallery_Activated;
         }
 
         private void GalleryItems_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            UpdateInfo();
+        }
+
+        private void UpdateInfo() {
             Title = $"{Assets.i18n.Resources.wnd_gallery} — {GalleryItems.SelectedIndex + 1}/{GalleryItems.Items.Count}";
 
             AttachmentBase attachment = GalleryItems.SelectedItem as AttachmentBase;
             if (attachment != null) {
                 dbgi.Text = attachment.ToString();
                 var owner = CacheManager.GetNameAndAvatar(attachment.OwnerId);
-                OwnerAvatar.IsVisible = owner != null;
-                if (owner != null) {
-                    string name = string.Join(" ", new string[] { owner.Item1, owner.Item2 });
-                    OwnerName.Text = name;
-                    OwnerAvatar.Background = attachment.OwnerId.GetGradient();
-                    OwnerAvatar.Initials = name;
-                    OwnerAvatar.SetImage(owner.Item3, OwnerAvatar.Width, OwnerAvatar.Height);
-                }
+                OwnerAvatar.IsVisible = owner.Item3 != null;
+                string name = string.Join(" ", new string[] { owner.Item1, owner.Item2 });
+                OwnerName.Text = name;
+                OwnerAvatar.Background = attachment.OwnerId.GetGradient();
+                OwnerAvatar.Initials = name;
+                if (owner.Item3 != null) OwnerAvatar.SetImage(owner.Item3, OwnerAvatar.Width, OwnerAvatar.Height);
 
                 if (attachment is Photo photo) {
                     Date.Text = photo.Date.ToHumanizedString(true);
@@ -62,23 +68,24 @@ namespace ELOR.Laney.Views.Media {
 
         private void Gallery_Activated(object sender, System.EventArgs e) {
             Activated -= Gallery_Activated;
-            if (items == null || items.Count == 0) {
+            if (_items == null || _items.Count == 0) {
                 Close();
                 return;
             }
-            target = target != null ? target : items[0];
+            _target = _target != null ? _target : _items[0];
 
+            GalleryItems.SelectionChanged += GalleryItems_SelectionChanged;
             new System.Action(async () => {
-                GalleryItems.ItemsSource = items;
-                await Task.Delay(100); // required.
-                GalleryItems.SelectedItem = target;
+                GalleryItems.ItemsSource = _items;
+                await Task.Delay(50); // required, without delay scrolling to target in FlipView not properly working.
+                GalleryItems.SelectedItem = _target;
             })();
+            if (_items.FirstOrDefault() == _target) UpdateInfo(); // required, because GalleryItems_SelectionChanged doesn't called after first loading (because default index is 0).
         }
 
         private void ImageDataContextChanged(object sender, System.EventArgs e) {
             Image image = sender as Image;
             IPreview item = image.DataContext as IPreview;
-            ZoomBorder zb = image.Parent as ZoomBorder;
 
             if (item is Photo photo) {
                 var p = photo.MaximalSizedPhoto;
@@ -104,8 +111,11 @@ namespace ELOR.Laney.Views.Media {
 
         private void ZoomBorder_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
             ZoomBorder zb = sender as ZoomBorder;
+            Control child = zb.Child;
+
             zb.Width = Width;
             zb.Height = Height;
+
             SizeChanged += (a, b) => {
                 zb.Width = b.NewSize.Width;
                 zb.Height = b.NewSize.Height;
