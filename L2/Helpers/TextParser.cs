7456fcccdb20e5d56ff56b88920c004655f9518c
Elorucov <elorucov@live.ru>
2025-10-26 21:22:15 +0400

    Code cleanup + removed deprecated and test features.

--- L2/Helpers/TextParser.cs
+++ L2/Helpers/TextParser.cs
@@ -1,6 +1,5 @@
 ﻿using Avalonia.Collections;
 using ColorTextBlock.Avalonia;
-using ELOR.Laney.Assets.i18n;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -96,10 +95,10 @@ namespace ELOR.Laney.Helpers {
         }
 
         private static CHyperlink BuildCHyperlinkForRTBStyle(string text, string link, CTextBlock rtb, Action<string> clickedCallback) {
-            CHyperlink hl = new CHyperlink(new List<CInline> { 
+            CHyperlink hl = new CHyperlink(new List<CInline> {
                 new CRun() {
                     Text = text
-                } 
+                }
             }) {
                 CommandParameter = link
             };
